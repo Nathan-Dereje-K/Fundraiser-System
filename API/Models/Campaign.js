bdd827bc7040a76a3b605513e361ad7e0c6a42f5
Nathan <natidare61@gmail.com>
2025-04-15 21:04:24 +0300

    Refactor Campaign schema to clarify userId field; remove commented-out creator field for cleaner code

--- API/Models/Campaign.js
+++ API/Models/Campaign.js
@@ -49,11 +49,12 @@ const campaignSchema = new mongoose.Schema(
     },
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
-    // creator: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "User",
-    //   required: true,
-    // },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      // required: true,
+    },
 
     // donations: [
     //   {
@@ -61,6 +62,7 @@ const campaignSchema = new mongoose.Schema(
     //     ref: "Donation",
     //   },
     // ],
+
     image: [{ type: String }],
     video: [{ type: String }],
     document: [{ type: String }],
